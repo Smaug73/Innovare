4e939165f4e6abff29b5fdbe6a26698cb8e2df5a
Smaug73 <stefrondinella@live.it>
2020-10-09 16:57:54 +0200

    AggiuntaRichiestaConfInDash

--- innovare.dashboard/innovare/innovare/src/main/java/com/innovare/views/configurazione/ConfigurazioneView.java
+++ innovare.dashboard/innovare/innovare/src/main/java/com/innovare/views/configurazione/ConfigurazioneView.java
@@ -2,8 +2,20 @@ package com.innovare.views.configurazione;
 
 
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
 import com.innovare.utils.Role;
 import com.innovare.views.main.ContentView;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
@@ -19,6 +31,45 @@ public class ConfigurazioneView extends Div {
     public ConfigurazioneView() {
         setId("config-view");
         add(new Label("Content placeholder"));
+        
+        
+        URIBuilder builder = new URIBuilder();
+		builder.setScheme("http").setHost("localhost:8888").setPath("/actualConfiguration");
+		URI uri = null;
+		try {
+			uri = builder.build();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+
+		HttpGet get = new HttpGet(uri);
+		CloseableHttpClient client = HttpClients.createDefault();
+
+		try(CloseableHttpResponse response = client.execute(get)){
+			String status = response.getStatusLine().toString();
+			System.out.println(status);
+			if(status.contains("200")) {
+				System.out.println("OK!");
+				System.out.println(response.getAllHeaders().toString());
+				//add(new Label("Content placeholder"));
+				//vaadinSession.setAttribute(ATTRIBUTE_USERNAME , username);
+				//vaadinSession.setAttribute(ATTRIBUTE_IS_AUTH , Boolean.TRUE);
+				//UI.getCurrent().navigate(parameter);
+		
+			}else
+			{
+				System.out.println("Errore");
+				//loginForm.setError( true );
+			}
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Errore");
+		}
+
+	
+		
     }
 
 }
