7ae7a518d0006b951af80df4a6b7e7d2b9434be4
Smaug73 <stefrondinella@live.it>
2021-06-01 18:21:00 +0200

    Modifica script python rele

--- innovare.utils/pythonScript/irrigationCommand.py
+++ innovare.utils/pythonScript/irrigationCommand.py
@@ -2,36 +2,14 @@ import PiRelay
 import time
 
 
-if len(sys.argv) != 3: 
+if len(sys.argv) != 3:
     sys.exit('Errore: inserire on oppure off come parametro')
-try:
-    nomeScript,comando = sys.argv
+nomeScript,comando,id_rel = sys.argv
 
-    r1 = PiRelay.Relay("RELAY1")
-    
-    if comando=="on":
+r1 = PiRelay.Relay("RELAY"+id_rel)
+
+if comando=="on":
         r1.on()
 
-    if comando=="off":
+if comando=="off":
         r1.off()
-
-    '''
-    time.sleep(2)
-    r1.off()
-    time.sleep(2)
-
-    r2.on()
-    time.sleep(2)
-    r2.off()
-    time.sleep(2)
-
-    r3.on()
-    time.sleep(2)
-    r3.off()
-    time.sleep(2)
-
-    r4.on()
-    time.sleep(2)
-    r4.off()
-    time.sleep(2)
-    '''
